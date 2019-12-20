2636cb63af225436ecb7a828692e6251ff7be06f
MCarvalho <178carvalho@gmail.com>
2019-12-20 18:00:07 -0300

    Inicio GYMPOINT FRONTEND

---
+++ src/pages/_layouts/auth/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: rgb(247, 155, 167);
+  background: linear-gradient(
+    135deg,
+    rgba(247, 155, 167, 1) 0%,
+    rgba(238, 77, 99, 1) 100%
+  );
+`;
