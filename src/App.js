0c1f1ad2063d38bedf5f409ebdcb17db26f913df
MCarvalho <178carvalho@gmail.com>
2019-12-20 23:11:17 -0300

    Redux - saga - zzz

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './config/ReactotronConfig';
 
 import history from './services/history';
 import Routes from './routes';
+
+import store from './store';
 
 import GlobalStyle from './styles/global';
 
