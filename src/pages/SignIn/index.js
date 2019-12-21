a635dec27f3f6e8032eb995513dd7bcc42243694
MCarvalho <178carvalho@gmail.com>
2019-12-20 22:42:39 -0300

    SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '../../assets/images/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('Digite um email'),
+  password: Yup.string()
+    .min(6, 'Senha deve ter no mínimo 6 digitos')
+    .required('Digite sua senha'),
+});
 
 export default function SignIn() {
-  return <div />;
+  function handleSubmit(data) {}
+
+  return (
+    <>
+      <img src={logo} alt="GYMPOINT" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <strong>EMAIL </strong>
+        <Input name="email" type="email" placeholder="Digite seu email" />
+        <strong>SENHA </strong>
+        <Input name="password" type="password" placeholder="Digite sua senha" />
+        <button type="submit">Acessar</button>
+      </Form>
+    </>
+  );
 }
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
@@ -8,4 +9,77 @@ export const Wrapper = styled.div`
     rgba(247, 155, 167, 1) 0%,
     rgba(238, 77, 99, 1) 100%
   );
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  max-width: 360px;
+  background: rgb(238, 77, 99);
+  background: linear-gradient(
+    135deg,
+    rgba(238, 77, 99, 1) 0%,
+    rgba(247, 155, 167, 1) 100%
+  );
+  border-radius: 8px;
+  padding: 30px;
+  border: 1px solid #eee;
+
+  img {
+    text-align: center;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    strong {
+      font-size: 16px;
+      color: #fff;
+      margin-bottom: 5px;
+    }
+
+    input {
+      border: 0;
+      height: 45px;
+      padding: 0 15px;
+      margin: 0 0 10px;
+      color: #ee4d63;
+      font-weight: bold;
+      border-radius: 4px;
+      background: rgba(255, 255, 255, 0.6);
+
+      &::placeholder {
+        color: rgba(238, 77, 99, 0.7);
+      }
+
+      &:focus {
+        border: 1px solid #eee;
+      }
+    }
+
+    button {
+      margin: 5px 0 0;
+      height: 45px;
+      font-weight: bold;
+      background: #eee;
+      border: 0;
+      color: #ee4d63;
+      border-radius: 4px;
+      font-size: 16px;
+
+      &:hover {
+        background: ${darken(0.05, '#eee')};
+        transform: translateY(-2px);
+        transition: all 0.11s;
+      }
+    }
+  }
 `;
